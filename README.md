a89ea1c97f82b8d78242986f8ddd4e0ba20dd61e
Tam Jid <tamjid.mdh@gmail.com>
2023-09-20 18:06:53 +0600

    style fix, readme, license files

---
+++ README.md
@@ -0,0 +1,56 @@
+# Custom JWT Expiration
+
+![Version](https://img.shields.io/badge/version-1.0.5-brightgreen)
+![WordPress](https://img.shields.io/badge/WordPress-5.8%2B-blue)
+![PHP](https://img.shields.io/badge/PHP-7.0%2B-blue)
+
+Custom JWT Expiration is a WordPress plugin that allows administrators to set the JWT (JSON Web Token) expiration time manually in seconds. This plugin provides a user-friendly interface for configuring the JWT expiration time to suit your specific needs.
+
+## Features
+
+- Set JWT expiration time in seconds.
+- Easy-to-use settings page.
+- Suitable for developers and administrators.
+
+## Installation
+
+1. Download the `custom-jwt-expiration.zip` file from the [latest release](https://github.com/tee-jaay/wp-jwt-custom-expiration/releases).
+2. Log in to your WordPress admin dashboard.
+3. Navigate to "Plugins" > "Add New."
+4. Click the "Upload Plugin" button and upload the `custom-jwt-expiration.zip` file.
+5. Activate the plugin.
+
+## Usage
+
+1. After activating the plugin, go to "Settings" > "JWT Expiration" in your WordPress admin dashboard.
+2. You can now set the JWT expiration time in seconds on the settings page.
+3. Click "Save Changes" to apply your custom JWT expiration time.
+
+## Contributing
+
+If you find any issues or have suggestions for improvements, please [open an issue](https://github.com/tee-jaay/wp-jwt-custom-expiration/issues) on GitHub.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Author
+
+- [Tamdjid](https://teejaay.me)
+
+## Support
+
+For support or inquiries, please contact us through our [website](https://teejaay.me/contact).
+
+## Changelog
+
+### 1.0.1
+
+- Added the ability to set JWT expiration time from the WordPress dashboard.
+- Bug fixes and performance improvements.
+
+### 1.0.0
+
+- Initial release.
+
+Enjoy using Custom JWT Expiration!
--- custom-jwt-expiration.php
+++ custom-jwt-expiration.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * Plugin Name: Custom JWT Expiration
- * Description: Allows the admin to set the JWT expiration token manually.
- * Version: 1.0.0
+ * Description: Allows the admin to set the JWT expiration time manually in seconds.
+ * Version: 1.0.1
  * Author: Tamjid
  * Author URI: https://teejaay.me
+ * License: MIT License
+ * License URI: https://teejaay.me/wp-content/plugins/custom-jwt-expiration/license.txt
  */
 
-// Add the JWT expiration setting to the admin menu
+// Add the JWT expiration setting to the Settings menu
 function custom_jwt_expiration_menu()
 {
     add_options_page(
@@ -26,17 +28,34 @@ function custom_jwt_expiration_settings_page()
     if (!current_user_can('manage_options')) {
         return;
     }
+
+    if (isset($_POST['custom_jwt_expiration'])) {
+        update_option('custom_jwt_expiration', absint($_POST['custom_jwt_expiration']));
+    }
+
+    $customExpiration = get_option('custom_jwt_expiration', 60);
+
     ?>
     <div class="wrap">
         <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-        <form method="post" action="options.php">
+        <form method="post" action="">
+            <div class="custom-jwt-expiration-settings-field">
+                <label for="custom_jwt_expiration">JWT Expiration (seconds):</label>
+                <input type="number" name="custom_jwt_expiration" id="custom_jwt_expiration"
+                    value="<?php echo esc_attr($customExpiration); ?>" />
+                <p class="description">Set the JWT expiration time in seconds.</p>
+            </div>
             <?php
-            settings_fields('custom_jwt_expiration');
-            do_settings_sections('custom_jwt_expiration');
             submit_button();
             ?>
         </form>
     </div>
+    <style>
+        /* Adjust the position of the form on the page */
+        .wrap .custom-jwt-expiration-settings-field {
+            margin-top: 20px;
+        }
+    </style>
     <?php
 }
 
@@ -50,28 +69,16 @@ function custom_jwt_expiration_init()
             'type' => 'integer',
             'description' => 'Expiration time in seconds',
             'sanitize_callback' => 'absint',
-            'default' => 300,
+            'default' => 60,
         )
     );
 }
 add_action('admin_init', 'custom_jwt_expiration_init');
 
-// Modify the JWT expiration time
+// Set the JWT expiration time from the saved option
 function custom_jwt_expiration_filter($expiration)
 {
-    $custom_expiration = get_option('custom_jwt_expiration', 3600);
+    $custom_expiration = get_option('custom_jwt_expiration', 60);
     return $custom_expiration;
 }
 add_filter('graphql_jwt_auth_expire', 'custom_jwt_expiration_filter', 10);
-
-// Add a function to display the custom JWT expiration setting on the settings page
-function custom_jwt_expiration_settings_field()
-{
-    $custom_expiration = get_option('custom_jwt_expiration', 3600);
-    ?>
-    <input type="number" name="custom_jwt_expiration" id="custom_jwt_expiration"
-        value="<?php echo esc_attr($custom_expiration); ?>" />
-    <p class="description">Set the JWT expiration time in seconds.</p>
-    <?php
-}
-add_action('admin_init', 'custom_jwt_expiration_settings_field');
